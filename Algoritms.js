aeeb4b428aa7aeefc440794a0307c9100bd93ac5
Dilshodbek <khamidovdilshodbekk@gmail.com>
2022-05-18 14:31:26 +0500

    Some coding 18.05.2022

--- Algoritms.js
+++ Algoritms.js
@@ -14,8 +14,27 @@ function sumZero(arr) {
     }
 }
 
-const arr = [1, 2, 3, 4, 5 - 1, -2, -3, -4];
-const result = sumZero(arr);
-console.log(result);
+// const arr = [1, 2, 3, 4, 5 - 1, -2, -3, -4];
+// const result = sumZero(arr);
+// console.log(result);
 
-/**@author Dilshodbek Hamidov ,Nobody,17.05.2022,time 0.521 seconds 😱😱   */ 
+/**@author Dilshodbek Hamidov ,Nobody,17.05.2022,time 0.521 seconds 😱😱   */
+
+
+
+const memoization = (value) => {
+    let cache = {};
+    if (value in cache) {
+        return cache[value]
+    } else {
+        let result = value * value;
+        cache[result] = result;
+        return result;
+    }
+}
+const simple = (value) => {
+    return value * value;
+}
+// const test = memoization(1000);//=>time 0.11 seconds with
+// const sim = simple(1000);//=>time 0.12 seconds
+/**@author Dilshodbek Hamidov,currently Nobody,17.05.2022 in Davr Bank*/
